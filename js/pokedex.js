f40045e8d562b6bb657bbe4183e8bcf3f866d598
AntoineJouvance <antoinejou94@gmail.com>
2018-03-21 19:34:02 +0100

    1st commit

--- js/pokedex.js
+++ js/pokedex.js
@@ -6,3 +6,33 @@ pokeApp.constant('POKEAPI', 'http://pokeapi.co');
 pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
+
+pokeApp.controller('myCtrl',['$scope', '$log',
+    function($scope, $log){
+        $scope.id = '1';
+        $scope.$log = $log;
+        $scope.list = {
+            model: null,
+            poke :[
+                {id: '1', name: 'bulbasaur'},
+                {id: '2', name: 'ivysaur'},
+                {id: '3', name: 'venusaur'},
+                {id: '7', name: 'squirtle'},
+                {id: '8', name: 'Wartortle'},
+                {id: '9', name: 'Blastoise'},
+                {id: '150', name: 'Mewtwo'}
+
+            ]
+        };
+    }]);
+
+pokeApp.controller('httpCtrl', function($scope, $http) {
+    $http.get("https://pokeapi.co/api/v1/pokedex/")
+        .then(function(response) {
+            $scope.data = response.data;
+        });
+});
+
+pokeApp.factory("infoPokeFactory", function ($resource) {
+    return $resource("http://pokeapi.co/api/v1/pokemon/:id/");
+});
